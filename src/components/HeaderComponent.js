7be445b98d6871e0578b5e3c7e7d18734d6bbd0d
ouaf99 <56977578+ouaf99@users.noreply.github.com>
2021-03-23 20:27:02 +0100

    Update HeaderComponent.js

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -13,7 +13,7 @@ class HeaderComponent extends Component {
             <div>
                  <header>
                     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                    <div><a href="http://localhost:3000/" className="navbar-brand">User Management App</a></div>
+                    <div><a href="https://react-js-users-management-app.herokuapp.com/" className="navbar-brand">User Management App</a></div>
                     {/*<div><h2 className="navbar-brand">User Management App</h2></div>*/}
                     </nav>
                 </header>
